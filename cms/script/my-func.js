26c3cf1b3a8a3dff65981266af79a272e3f867ab
thuantruong00 <taithuan2000@gmail.com>
2023-12-27 12:44:08 +0700

    account, static-, static-noti, login

--- cms/script/my-func.js
+++ cms/script/my-func.js
@@ -9,6 +9,7 @@ $(".my-dropdown-button").click(function(){
    click_status=false
     let menu_id = $(this).attr("dropdown-menu-name");
     $(menu_id).toggleClass('d-none')
+    $(this).toggleClass('dropdown-btn-active')
     setTimeout(() => {
         click_status = true
     }, 1000);
@@ -17,6 +18,7 @@ $(".my-dropdown-button").click(function(){
 $(window).click(function(){
     if(click_status){
         console.log("w")
+        $(".dropdown-btn-buble").removeClass('dropdown-btn-active')
         $(".dropdown-child-buble").addClass('d-none')
     }
 })
